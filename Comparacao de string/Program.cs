fa40275426f1542e0f392e61aff1f3badf9fe631
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-12 16:42:15 -0300

    Exemplo de comparação

---
+++ Comparacao de string/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparacao_de_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor inicial");
+            var primeiroValor = Console.ReadLine();//Obtem todos os valores digitados antes de ser precionado enter 
+                                                   //dentro de um a linha, esta variavel representa o primeiro valor digitado.
+                                                   //ReadLine sempre retorna uma string
+                                                   // Como ele e um metodo ele precisa dos indicadores "()"
+            Console.WriteLine("informe um valor para comparação");
+            var segundoValor = Console.ReadLine();//Realiza o mesmo como descrito nas linhas superiores mas 
+                                                  //agora ele pega o segundo valo, o quela usaremos para comparar
+
+
+            if (primeiroValor == segundoValor)//Comparação exata de valores os mesmo não podem ter nenhuma diferença 
+                Console.WriteLine("O primeiro valor é exatamente igual ao segundo valor.");
+            else
+                Console.WriteLine("Os valores não são exatamente iguais.");
+
+
+
+            if (segundoValor.Contains(primeiroValor))//Verificação se o mesmo contem uma informação dentro da outra 
+                Console.WriteLine("O primeiro valor contem em algum local o segundo valor.");
+            else
+                Console.WriteLine("Não existe nenhum valor dentro do texto informado.");
+            //Ou seja ana existe dentro de "Ban'ana'"
+
+
+
+            if (segundoValor.IndexOf(primeiroValor) > -1)//Realiza uma busca dentro da string e retorna a primeira posição
+                Console.WriteLine($"O primeiro existe dentro do segundo valor pos:{segundoValor.IndexOf(primeiroValor)}");
+            else
+                Console.WriteLine($"infelixmente ele não retornou nada, pos:{segundoValor.IndexOf(primeiroValor)}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PodeBeber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodeBeber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um nome:"); //WriteLine esquevre informação e quebra a linha depois.
+            var nome = Console.ReadLine(); // ReadLine le uma linha inteira com mais de 1 informação
+
+            Console.WriteLine("Informe a idade:");
+            int.TryParse(Console.ReadLine(), out int idade);//TryParse espera uma string  e depois da virgula para separar
+                                                            //o os parametros temos a saida do valor inserido.  
+                                                            // colocando "out int" temos a criação da variavel
+                                                            // colocando apenas "out" precisamos informar uma variavel existente.
+
+            if(idade > 18)
+            {
+                Console.WriteLine($"Bora {nome} Sextou carai bora, arranca o teto da baia que vou chegar vuanduuuuuuu!!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine($"{nome}, infelizmente você não esta apto a beber.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
