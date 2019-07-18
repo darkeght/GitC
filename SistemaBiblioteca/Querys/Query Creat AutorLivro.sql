313257b2dbccbd176d8daa627d080d7d6ab3781b
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 17:25:46 -0300

    Comit Aula de hoje muito legal

---
+++ SistemaBiblioteca/Querys/Query Creat AutorLivro.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[LivroAutor]
+(
+    [Livro] INT NOT NULL, 
+    [Autor] INT NOT NULL, 
+
+    CONSTRAINT   [FK_LivroAutor_Livros] 
+	FOREIGN KEY ([Livro]) 
+	REFERENCES   [Livros]([Id]),
+
+	CONSTRAINT   [FK_LivroAutor_Autores] 
+	FOREIGN KEY ([Autor]) 
+	REFERENCES   [Autores]([Id]),
+
+)
---
+++ SistemaBiblioteca/Querys/Query Creat Editor.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Editora]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Nome] VARCHAR(200) NOT NULL,
+    [Descricao] VARCHAR(1000)
+)
