08e0a095594ac8c2753cedf6c726feb121525fc8
Felipe Machado <contato@usysweb.coom.br>
2019-07-22 07:15:15 -0300

    Comit

---
+++ ConsoleOperationsOverride/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste T = new Teste(5,5);
+
+            T += T;
+
+            var resultado1 = T.A + T.B;
+
+            T++;
+
+            var resultado2 = T.A;
+        }
+    }
+
+    public class Teste
+    {
+        public int A { get; set; } = 0;
+        public int B { get; set; } = 0;
+
+        public Teste()
+        {
+
+        }
+        public Teste(int a,int b)
+        {
+            A = a;
+            B = b;
+        }
+        public static Teste operator +(Teste op1, Teste op2)
+        {
+            Teste C = new Teste();
+            C.A = op1.A * op2.B;
+            return C;
+        }
+
+        public static Teste operator ++(Teste op1)
+        {
+            op1.A = op1.A * 2;
+            return op1;
+        }
+    }
+
+
+
+}
