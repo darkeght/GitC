c48dd4b14eca7e1e5a8b7a27ec1c8f9b551ff0e2
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-20 10:23:03 -0300

    Commit btn  editar dinamico

--- 12-08-19_16-08-19/WEBAPIRESTFULL/SITETESTAPIRESTFULL/js/utils.js
+++ 12-08-19_16-08-19/WEBAPIRESTFULL/SITETESTAPIRESTFULL/js/utils.js
@@ -1,21 +1,30 @@
+var urlBaseApi = "http://localhost:59271/Api/"
+
+function buildUrlApi(sendpost,id = ''){
+    if(id !== '')
+        id = '/'+id;
+
+    return urlBaseApi + sendpost + id;
+}
+
 jQuery(document).ready(function(){
     /* Indica que o evento submit do form irá realizar esta ação agora*/
     jQuery('.form-post').submit(function(){
         /* Neste contesto 'this' representa o form deste ID  #myform */  
-        var Id = $($(this)[0][1]).val();            
+        var id = $($(this)[0][1]).val();            
         var dados = $(this).serialize();
         var sendpost = $(this).attr('send-post');
         var callStr  = $(this).attr('call');
         var method = "POST";
 
-        if(Id !== "") {
+        if(id !== "") {
             method = "PUT";
-            Id = "/"+Id;
+            id = "/"+id;
         }
 
          var settings = {
           "crossDomain": true,
-          "url": "http://localhost:59271/Api/"+sendpost + Id,
+          "url": buildUrlApi(sendpost,id),
           "method": method,
           "headers": {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -41,7 +50,7 @@ function SetGridClickEvents(){
 
         var settings = {
             "crossDomain": true,
-            "url": "http://localhost:59271/Api/"+ sendpost +"/"+ id,
+            "url":  buildUrlApi(sendpost,id),
             "method": "DELETE",
             "headers": {
               "Content-Type": "application/x-www-form-urlencoded",
@@ -52,5 +61,28 @@ function SetGridClickEvents(){
           $.ajax(settings).done(function (response) {
               GetMethod(null);
           });
+    });
+
+    $('.btn-editing-event').click(function(){
+         var id = $(this).attr('value');
+         var sendpost = $(this).attr('send-post');
+
+         var settings = {
+          "crossDomain": true,
+          "url": buildUrlApi(sendpost,id),
+          "method": "GET",
+          "headers": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "*/*"
+          }
+        }
+
+      $.ajax(settings).done(function (response) {
+          $.each(response,function(index,value){
+              /* teste Property and value*/
+              $('input[name="'+ index +'"]').val(value);
+          });
+      });
+
     });
 }
